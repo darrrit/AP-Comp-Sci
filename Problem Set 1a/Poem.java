126eb490b78d7b77d6432063be1acc6b15202afd
Naomi Wolfman <81047700+darrrit@users.noreply.github.com>
2024-09-10 12:29:36 -0700

    Add files via upload

---
+++ Problem Set 1a/Poem.java
@@ -0,0 +1,17 @@
+/** AP Computer Science
+ * Our First Example
+ * By Naomi Wolfman
+ */
+
+
+
+public class Poem {
+
+    public static void main (String[] args) {
+        //output some text to terminal
+        System.out.println("Evital Gracie");
+        System.out.println("i like your whispy hair strands");
+        System.out.println("they frame your face well");
+    }
+    
+}
---
+++ Problem Set 1a/studentQuestions.java
@@ -0,0 +1,38 @@
+/** 
+ * i am naomi
+ * sept. 9 2024
+ * we do be scanning
+ */
+
+import java.util.Scanner;
+
+public class SickestBurn {
+    public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.print("What's your favourite course?: ");
+    String favCourse = input.nextLine();
+   
+    
+    System.out.print("What's your favourite show?: ");
+    String favShow = input.nextLine();
+   
+    
+    System.out.print("How many hours do you spent studying each day?: ");
+    int studyHours = input.nextInt();
+
+    System.out.print("How many hours do you spend watching TV each day?: ");
+    int TVHours = input.nextInt();
+    
+    System.out.println(" ");
+    
+    System.out.println("Summary:");
+    System.out.println("Your favourite course is: " + favCourse + ".");
+    System.out.println("Your favourite show is: " + favShow + ".");
+    System.out.println("You spend " + studyHours + " hours studying each day" + ".");
+    System.out.println("You spend " + TVHours + " watching TV each day.");
+    System.out.println("In total, you spend " + (studyHours + TVHours) + " studying and watching TV each day.");
+    
+    input.close();
+    
+    }
+}
