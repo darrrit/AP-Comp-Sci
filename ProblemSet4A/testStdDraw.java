e50592beaf827281762b45977f467709c42b8dcf
Naomi Wolfman <81047700+darrrit@users.noreply.github.com>
2024-10-22 13:54:33 -0700

    Add files via upload

--- ProblemSet4A/testStdDraw.java
+++ ProblemSet4A/testStdDraw.java
@@ -1,6 +1,33 @@
 public class testStdDraw {
     public static void main(String[] args) {
         StdDraw.setScale(0, 500);
-        
+        circles();
+    }
+
+    public static void grid() {
+        for (int i = 0; i < 500; i += 20) {
+            StdDraw.line(0, i, 500, i);
+            StdDraw.line(i, 0, i, 500);
+        }
+    }
+
+    public static void curve() {
+        for (int i = 0; i < 500; i += 20) {
+            StdDraw.line(500, i, i, 0);
+            StdDraw.line(0, 500-i, 500-i, 500);
+        }
+    }
+
+    public static void circles() {
+        boolean EorO = true;
+        for (int i = 360; i > 0; i -= 10) {
+            if (!EorO) {
+                StdDraw.filledCircle(500/2, 500/2, i);
+            } else {
+                StdDraw.circle(500/2, 500/2, i);
+            }
+            
+        }
+        EorO = !EorO;
     }
 }
