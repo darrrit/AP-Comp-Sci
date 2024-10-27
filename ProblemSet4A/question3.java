d85d082491d18641e1721e49777fd5950cefcd6c
darrrit <81047700+darrrit@users.noreply.github.com>
2024-10-26 22:14:46 -0700

    fixing the stupid triangle

--- ProblemSet4A/question3.java
+++ ProblemSet4A/question3.java
@@ -3,23 +3,24 @@ public class question3 {
         StdDraw.setScale(0, 1);
         OrderedPair topLeft = new OrderedPair(0, 1);
         OrderedPair topRight = new OrderedPair(1, 1);
-        OrderedPair bttm = new OrderedPair(0.5, 1);
+        OrderedPair bttm = new OrderedPair(0.5, 0);
         StdDraw.point(bttm.X, bttm.Y);
         StdDraw.point(topLeft.X, topLeft.Y);
         StdDraw.point(topRight.X, topLeft.Y);
         OrderedPair rndm = new OrderedPair(Math.random(), Math.random());
-        for (int i = 0; i <10000; i++) {
-            int rnm = (int) Math.random()*3;
+        for (int i = 0; i <20000; i++) {
+            int rnm = (int) (Math.random()*3);
             if (rnm == 1) {
-            rndm.X = (rndm.X + topLeft.X / 2);
-            rndm.Y = (rndm.Y + topLeft.Y / 2);
+            rndm.X = ((rndm.X + topLeft.X) / 2);
+            rndm.Y = ((rndm.Y + topLeft.Y) / 2);
             } else if (rnm ==2) {
-            rndm.X = (rndm.X + topRight.X / 2);
-            rndm.Y = (rndm.Y + topRight.Y / 2);
+            rndm.X = ((rndm.X + topRight.X) / 2);
+            rndm.Y = ((rndm.Y + topRight.Y) / 2);
             } else {
-            
+            rndm.X = ((rndm.X + bttm.X) / 2);
+            rndm.Y = ((rndm.Y + bttm.Y) / 2);
             }
-            rndm
+            StdDraw.point(rndm.X, rndm.Y);
         }
     }
 }
