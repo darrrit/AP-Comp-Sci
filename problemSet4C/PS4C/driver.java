ffacaa56dbaac150b4d47c976c6acdbf17fd99d8
Naomi Wolfman <81047700+darrrit@users.noreply.github.com>
2024-10-29 13:13:02 -0700

    Add files via upload

---
+++ problemSet4C/PS4C/driver.java
@@ -0,0 +1,24 @@
+
+/**
+ * naomi wolfman
+ * 1-3
+ * oct. 28 2024
+ */
+import java.util.Scanner;
+public class driver
+{
+    public static void main (String[] args) {
+        System.out.println(Review.starRating("review.txt"));
+    }
+
+    static void testSV () {
+        int i = 0;
+        Scanner input = new Scanner(System.in);
+        while (i < 3) {
+            String testWord = input.nextLine();
+            System.out.println(Review.sentimentVal(testWord));
+            if (Review.sentimentVal(testWord) != 0) i++;
+        }
+    }
+    
+}
