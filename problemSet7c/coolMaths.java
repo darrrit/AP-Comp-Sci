4b5796d19b10a45341e9d50d9043582e1bce888d
darrrit <81047700+darrrit@users.noreply.github.com>
2024-12-03 20:20:45 -0800

    finish Pset 7c

--- problemSet7c/coolMaths.java
+++ problemSet7c/coolMaths.java
@@ -1,3 +1,9 @@
+/**
+ * Naomi Wolfman
+ * problem set 7c
+ * dec. 3 2024
+ */
+
 import java.util.ArrayList;
 
 public class coolMaths
@@ -6,8 +12,6 @@ public class coolMaths
 
     public coolMaths()
     {
-        // initialise instance variables
-
     }
 
     public static ArrayList<Integer> sieveOfEratosthenes(int n)
@@ -47,16 +51,30 @@ public class coolMaths
     
     public static ArrayList<Integer> addingBigInts(int a, int b) {
         // i get you would input them as lists as well but i dont wanna do that to myself while tesing
-        for (int i )
-        
+        ArrayList<Integer> aList = makeBigInt(a);
+        ArrayList<Integer> bList = makeBigInt(b);
+        ArrayList<Integer> ans = new ArrayList();
+        boolean carry = false;
+        for (int i = 0; i < aList.size() || i < bList.size(); i++) {
+            int digit = 0;
+            if (carry) digit++;
+            if (i < aList.size()) digit += aList.get(aList.size() - i - 1);
+            if (i < bList.size()) digit += bList.get(bList.size() - i - 1);
+            carry = digit/10 >= 1;
+            digit = digit%10;
+            ans.add(0, digit);
+        }
+        if (carry) ans.add(0, 1);
+        return ans;
     }
     
-    public static ArrayList<Integer> makeBigint(int a) {
+    public static ArrayList<Integer> makeBigInt(int a) {
         ArrayList<Integer> bigInt = new ArrayList();
         while (a > 0) {
-            bigInt.add(bigInt.size(), a%10);
+            bigInt.add(0, a % 10);
             a = a/10;
         }
+
         return bigInt;
     }
     
